86edbc952e2be16d03bf9f25ef0c985f781104cd
Nils Kriedner <nkriedner@googlemail.com>
2024-02-19 21:30:09 +0100

    Make express use static files + update image urls placeholder data

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const data = require("./data.json");
 
 const app = express();
 
+app.use(express.static("public"));
+
 app.listen(3000, () => {
     console.log("Server listening on port 3000.");
 });
