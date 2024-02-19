7a72706886b369b28042e4c19a4a40ba7acae9e5
Nils Kriedner <nkriedner@googlemail.com>
2024-02-19 21:38:52 +0100

    Create the / index route

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const data = require("./data.json");
 
 const app = express();
 
+// General settings & Middleware
 app.set("view engine", "pug");
 app.use(express.static("public"));
+
+// Routes
+app.get("/", (req, res) => {
+    console.log("GET request to / route...");
+    const projects = data.projects;
+    res.render("index", { projects });
+});
 
 app.listen(3000, () => {
     console.log("Server listening on port 3000.");
